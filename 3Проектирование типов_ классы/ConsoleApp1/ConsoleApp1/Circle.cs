d02ac9bff574d977d75d87d741ef4bc743bdc0b3
Miwoder <54844105+Miwoder@users.noreply.github.com>
2019-12-29 00:26:35 +0300

    Add files via upload

---
+++ 3Проектирование типов_ классы/ConsoleApp1/ConsoleApp1/Circle.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Circle
+    {
+        public string Name = Convert.ToString(Console.ReadLine());
+        public double xcen = Convert.ToDouble(Console.ReadLine());
+
+        public double Xcen
+        {
+            set
+            {
+                if (value < -50 || value > 50)
+                {
+                    Console.WriteLine("Координата Х задана неверно");
+
+                }
+                else
+                {
+                    xcen = value;
+                }
+            }
+            get
+            {
+                return xcen;
+            }
+        }
+
+        public double ycen = Convert.ToDouble(Console.ReadLine());
+
+        public double Ycen
+        {
+            set
+            {
+                if (value < -50 || value > 50)
+                {
+                    Console.WriteLine("Координата Y должны быть в пределе от -49 до 49");
+                }
+                else
+                {
+                    ycen = value;
+                }
+            }
+            get { return ycen; }
+        }
+        public double radius = Convert.ToDouble(Console.ReadLine());
+        public double Radius
+        {
+            set
+            {
+                if (value < 0 || value > 11)
+                {
+                    Console.WriteLine("Радиус должен быть в пределе от 1 до 10");
+                }
+                else
+                {
+                    radius = value;
+                }
+            }
+            get { return radius; }
+        }
+
+        public double Length()
+        {
+            double length = Math.PI * 2 * radius;
+            return length;
+        }
+        public double Square()
+        {
+        double square = Math.PI * Math.Pow(radius, 2);
+            return square;
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"Имя: {Name}  X: {xcen}  Y: {ycen}  Радиус: {radius}  Площадь: {Square()}  Длина: {Length()}");
+        }
+    }
+}
---
+++ 3Проектирование типов_ классы/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle1 = new Circle();
+            circle1.GetInfo();
+
+            Circle circle2 = new Circle();
+            circle2.GetInfo();
+
+            Circle circle3 = new Circle();
+            circle3.GetInfo();
+
+            Circle[] cirArray = { circle1, circle2, circle3};
+
+            double now = 9999;
+            string name = "default";
+            foreach (Circle a in cirArray)
+            {
+                if (a.Square() < now)
+                {
+                    now = a.Square();
+                    name = a.Name;
+                }
+            }
+            Console.WriteLine($"Наименьшая площадь: {name}");
+
+            double now1 = 0;
+            string name1 = "default";
+            foreach (Circle a in cirArray)
+            {
+                if (a.Square() > now1)
+                {
+                    now1 = a.Square();
+                    name1 = a.Name;
+                }
+            }
+            Console.WriteLine($"Наибольшая площадь: {name1}");
+
+            if (circle1.xcen == circle2.xcen)
+            
+                {
+                    if (circle2.xcen == circle3.xcen)
+                    {
+                        Console.WriteLine("ЦЕнтры окружности 1,2 и 3 на одной прямой");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Центры окружности 1 и 2 на одной прямой");
+                    }
+                }
+            else if (circle3.xcen == circle1.xcen)
+                {
+                    Console.Write("Центры окружности 1 и 3 на одной прямой");
+                }
+
+                else if (circle1.ycen == circle2.ycen)
+                {
+                    if (circle2.ycen == circle3.ycen)
+                    {
+                        Console.WriteLine("Центры окружности 1,2,3 на одной прямой");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Центры окружности 1,2 на одной прямой");
+                    }
+                }
+                else if (circle1.ycen == circle3.ycen)
+                {
+                    Console.WriteLine("Центры окружности 1,3 на одной прямой");
+                }
+                else
+                {
+                    Console.Write("Ни одного центра окружности нет на одной прямой");
+                }
+            
+
+            Console.ReadKey();
+        }
+    }
+}
